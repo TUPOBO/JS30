0cbf5261090437d92088daa6c0711d198dfd9b70
TUPOBO <1028461931@qq.com>
2018-05-17 20:56:18 +0800

    refect 01

--- 01-JavaScript-Drum-Kit/v1-JavaScript/v1/script/script.js
+++ 01-JavaScript-Drum-Kit/v1-JavaScript/v1/script/script.js
@@ -1,11 +1,14 @@
-window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', playSound)
+
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
   const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
-  if (!audio) return // stop the function from running all together
-  audio.currentTime = 0 // rewind to the start
-  audio.play()
-  key.classList.add('playing')
-})
+  if (audio) {
+    audio.currentTime = 0 // rewind to the start
+    audio.play()
+    key.classList.add('playing')
+  }
+}
 
 const keys = document.querySelectorAll('.key')
 
@@ -14,6 +17,7 @@ keys.forEach((key) => {
 })
 
 function deleteClass (e) {
-  if (e.propertyName !== 'transform') return // stop it if it's not a transform 
-  this.classList.remove('playing')
+  if (e.propertyName === 'transform') {
+    this.classList.remove('playing')
+  }// stop it if it's not a transform
 }
