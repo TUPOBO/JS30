99af9961fd9d59660098074fed90e203f44fc01b
TUPOBO <1028461931@qq.com>
2018-05-17 08:07:16 +0800

    simply finish js and add html

---
+++ 01-JavaScript-Drum-Kit/v1-JavaScript/src/index.js
@@ -0,0 +1,5 @@
+let vid = document.getElementById('text')
+console.log('====================================');
+console.log('fdsafds');
+console.log('====================================');
+vid.style.color = 'red'
